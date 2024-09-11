a0575aaa60861b1cdbf82d71f9be00b54500d50d
lRaphael <107645937+raphagamedev@users.noreply.github.com>
2024-09-11 20:29:21 -0300

    Add files via upload

---
+++ modulo de produto/incluir_comp_de_grade.cy.js
@@ -0,0 +1,66 @@
+describe('logando no illi', () => {
+  it('passes', () => {
+    cy.visit('http://hmg-premium.central.illimitar.com.br')
+
+    // ---------------------------------------------
+    //#############################################
+    //            LOGIN DE USUÁRIO
+     
+
+    // Preenchendo o campo de login
+    cy.get('[name="login"]').type('raphael');
+
+    // Executando JavaScript para definir o valor do campo de senha
+    cy.get('[name="senha"]').then(($input) => {
+      $input.val('i123').trigger('change');
+    });
+
+    cy.wait(1000);
+
+    // Clicando no botão de entrada forçando o clique mesmo que o botão esteja desativado
+    cy.contains('button', 'Entrar').click({force: true});
+    // Clicar no botão de entrada novamente
+    cy.contains('button', 'Entrar').click();
+
+    
+    // ---------------------------------------------
+    //#############################################
+    //            MODULO PRODUTO
+   
+    cy.wait(10000);
+
+    cy.get('#button-1605-btnIconEl').click({force: true});
+
+
+    // ---------------------------------------------
+    //#############################################
+    //           MODULO COMPOSIÇÃO DE GRADE
+
+    cy.wait(5000);
+    cy.get('#button-1610-btnInnerEl').click({force: true});
+    // ---------------------------------------------
+    //clica no botão "incluir"
+    cy.wait(5000);
+    cy.get('#button-1729-btnInnerEl').click({force: true});
+
+    // ---------------------------------------------
+    //adiciona o nome desejado no campo "Nome"
+    cy.wait(5000);
+    cy.get('#textfield-1763-inputEl').type('veio_do_cy');
+
+
+    //------------------------------------------------
+    //adiciona o tipo Vertical ou Horizontal no campo "Tipo" X -> Horizontal  Y -> Vertical
+    cy.get('#abstractcombo-1764-inputEl').type('Y'); //vertical
+    //cy.get('#abstractcombo-1764-inputEl').type('X'); //Horizontal
+    cy.wait(5000);
+
+    //---------------------------------------------
+    //salva o processo
+    
+    cy.get('#button-1767-btnIconEl').click({force: true});
+
+    //fim do processo de incluir uma grade
+    
+  })
+})
---
+++ modulo de produto/incluir_departamento_do_produto.cy.js
@@ -0,0 +1,65 @@
+describe('logando no illi', () => {
+    it('passes', () => {
+      cy.visit('http://hmg-premium.central.illimitar.com.br')
+  
+      // ---------------------------------------------
+      //#############################################
+      //            LOGIN DE USUÁRIO
+       
+  
+      // Preenchendo o campo de login
+      cy.get('[name="login"]').type('raphael');
+  
+      // Executando JavaScript para definir o valor do campo de senha
+      cy.get('[name="senha"]').then(($input) => {
+        $input.val('i123').trigger('change');
+      });
+  
+      cy.wait(1000);
+  
+      // Clicando no botão de entrada forçando o clique mesmo que o botão esteja desativado
+      cy.contains('button', 'Entrar').click({force: true});
+      // Clicar no botão de entrada novamente
+      cy.contains('button', 'Entrar').click();
+  
+      
+      // ---------------------------------------------
+      //#############################################
+      //            MODULO PRODUTO
+     
+      cy.wait(10000);
+  
+      cy.get('#button-1605-btnIconEl').click({force: true});
+  
+  
+      // ---------------------------------------------
+      //#############################################
+      //     MODULO DE DEPARTAMENTO DO PRODUTO
+
+      cy.get('#button-1616-btnInnerEl').click({force: true});
+
+      // ---------------------------------------------
+      //clica no botão "incluir"
+      cy.wait(5000);
+      cy.get('#button-1750-btnInnerEl').click({force: true});
+
+      //---------------------------------------------
+      //adiciona o nome desejado no campo "Codigo"
+    
+      cy.wait(5000);
+      cy.get('#numberfield-1822-inputEl').type('1');
+
+      // ---------------------------------------------
+      //adiciona o nome desejado no campo "Nome"
+      cy.wait(5000);
+      cy.get('#textfield-1823-inputEl').type('DEPARTAMENTO_VEIO_DO_CY');
+
+      //---------------------------------------------
+      //salva o processo
+    
+      cy.get('#button-1830-btnIconEl').click({force: true});
+
+      //fim do processo de incluir uma caracteristica
+      
+    })
+  })
